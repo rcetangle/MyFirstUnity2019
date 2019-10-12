00c05c389f1048907b9ee3fe3becfb0b969d4b78
Anice <rcetangle@qq.com>
2019-10-12 19:20:47 +0800

    clock

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+	public Transform hourTransform;
+	public Transform minutTransform;
+	public Transform secTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+     	Debug.Log("START"+DateTime.Now.Hour);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
